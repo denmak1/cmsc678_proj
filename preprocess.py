e95c97363d4eab50894b100e02e32a2ba0cea675
denmak1 <denmakmak1@gmail.com>
2019-05-13 20:27:05 -0400

    trying with Hough lines

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,91 @@ import sys
 import cv2
 from matplotlib import pyplot as plt
 
+def draw_lines(img, edges, color=[0, 0, 255], thickness=3):
+
+  lines = cv2.HoughLinesP(
+    edges,
+    rho=6,
+    theta=np.pi / 60,
+    threshold=160,
+    lines=np.array([]),
+    minLineLength=40,
+    maxLineGap=25
+  )
+
+  # If there are no lines to draw, exit.
+  if lines is None:
+    return
+
+  # Make a copy of the original image.
+  img = np.copy(img)
+
+  # Create a blank image that matches the original in size.
+  line_img = np.zeros((img.shape[0], img.shape[1], 3),  dtype=np.uint8,)
+
+  # Loop over all lines and draw them on the blank image.
+  for line in lines:
+    for x1, y1, x2, y2 in line:
+      cv2.line(line_img, (x1, y1), (x2, y2), color, thickness)
+
+  print(line_img.shape)
+  print(img.shape)
+
+  img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+  # Merge the image with the lines onto the original.
+  img = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
+
+  # Show keypoints
+  cv2.imshow("lines", img)
+  cv2.waitKey(0)
+
+  # Return the modified image.
+  return img
+# END draw_lines
+
+def blob_detect(img):
+  # Setup SimpleBlobDetector parameters.
+  params = cv2.SimpleBlobDetector_Params()
+
+  # Change thresholds
+  params.minThreshold = 50
+  params.maxThreshold = 200
+
+  # Filter by Area.
+  params.filterByArea = True
+  params.minArea = 15
+
+  # Filter by Circularity
+  params.filterByCircularity = True
+  params.minCircularity = 0.1
+
+  # Filter by Convexity
+  params.filterByConvexity = True
+  params.minConvexity = 0.87
+
+  # Filter by Inertia
+  params.filterByInertia = True
+  params.minInertiaRatio = 0.001
+
+  # Create a detector with the parameters
+  detector = cv2.SimpleBlobDetector_create(params)
+ 
+  # Detect blobs.
+  keypoints = detector.detect(img)
+ 
+  img_keypoints = \
+    cv2.drawKeypoints(img,
+                      keypoints,
+                      np.array([]),
+                      (0, 0, 255),
+                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+  # Show keypoints
+  cv2.imshow("Keypoints", img_keypoints)
+  cv2.waitKey(0)
+
+# overlay should be grayscale, img should be rgb
 def alpha_blend(overlay, img):
   img = np.float32(img)
   print(overlay.shape)
@@ -29,6 +114,8 @@ def alpha_blend(overlay, img):
  
   cv2.imshow("cvt", cvt)
   cv2.waitKey(0)
+
+  return cvt
 # END alpha_blend
 
 def adaptive_thresh(img):
@@ -62,11 +149,11 @@ def main():
 
   # read image
   img = cv2.imread(img_path)
-  img_bw = cv2.imread(img_path, 0)
+  img_bw = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+  blob_detect(img_bw)
 
   #t, img = cv2.threshold(img_bw, 200, 255, cv2.THRESH_BINARY_INV)
-
-  print(img)
 
   cv2.imshow("image", img)
   cv2.waitKey(0)
@@ -89,8 +176,12 @@ def main():
 
   # erosion
   erosion = cv2.erode(edges_inv, kernel, iterations=2)
+  erosion = cv2.bitwise_not(erosion)
+
   cv2.imshow("erosion", erosion)
   cv2.waitKey(0)
+
+  draw_lines(img_bw, erosion)
 
   # dilation
   dilation = cv2.dilate(edges, kernel, iterations=10)
